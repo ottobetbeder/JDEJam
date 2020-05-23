0ba40b700bf8fb2a4e103fc3eb81ca1bd1074647
Otto <ottobetbeder@yahoo.com.ar>
2020-05-23 14:14:13 -0300

    Added GameManager and the ability to lose. you can turn this off in the GameManager

---
+++ Assets/Code/BaseManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseManager : MonoBehaviour
+{
+    public Action BaseHurt;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            if (BaseHurt != null)
+            {
+                BaseHurt();
+            }
+        }
+    }
+}
---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int lives;
+    public BaseManager playerBase;
+    public Text livesText;
+    public GameObject GameOverPanel;
+
+    public bool CanLose = true;
+
+    private void Start()
+    {
+        playerBase.BaseHurt += OnBaseHurt;
+        livesText.text = lives.ToString();
+
+    }
+
+    private void OnBaseHurt()
+    {
+        lives--;
+        if (lives <= 0)
+        {
+            livesText.text = "0";
+            if(CanLose)
+            {
+                GameOverPanel.SetActive(true);
+            StartCoroutine(ResetSceen());
+            }
+        }
+        else
+        {
+            livesText.text = lives.ToString();
+        }
+    }
+
+    IEnumerator ResetSceen()
+    {
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
