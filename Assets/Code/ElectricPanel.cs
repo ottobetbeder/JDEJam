2ddf0ea127b4b6c870087450963f689802b99066
Otto <ottobetbeder@yahoo.com.ar>
2020-05-24 15:08:48 -0300

    Electric Panels now make sound

--- Assets/Code/ElectricPanel.cs
+++ Assets/Code/ElectricPanel.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ElectricPanel : MonoBehaviour
 {
+    public AudioSource electricity;
+
     public void TurnPanelOnFor(float segs)
     {
         this.gameObject.SetActive(true);
+        electricity.Play();
         StartCoroutine(DeactivatePanelAfterTime(segs));
     }
 
     IEnumerator DeactivatePanelAfterTime(float segs)
     {
         yield return new WaitForSeconds(segs);
+        electricity.Stop();
         this.gameObject.SetActive(false);
     }
 }
