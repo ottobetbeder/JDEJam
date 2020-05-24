425d0394e58e5d27a98ce365c1450af3cf235584
Otto <ottobetbeder@yahoo.com.ar>
2020-05-23 23:09:33 -0300

    Added the ability to dash pressing space to the player

---
+++ Assets/Code/CoolDownHability.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolDownHability
+{
+    public CoolDownHability(float cooldownTime)
+    {
+        this.cooldownTime = cooldownTime;
+    }
+
+    private float cooldownTime;
+    private float nextFireTime = 0;
+
+    public bool IsCoolDownCompleted
+    {
+        get
+        {
+            return Time.time > nextFireTime;
+        }
+    }
+
+    //this enable the cooldown, use it when you want to start a cooldown, like when you use an ability
+    public void SetCooldown()
+    {
+        nextFireTime = Time.time + cooldownTime;
+    }
+}
--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -8,19 +8,64 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb2d; 
     Vector2 movement;
 
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private CoolDownHability dashCooldown;
+    private float moveHorizontal;
+    private float moveVertical;
+    [SerializeField] private GameObject DashEffect;
+    [SerializeField] private float cooldownTime;
+    private bool isDashing;
+
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-        movement.x = Input.GetAxis("Horizontal");
-        movement.y = Input.GetAxis("Vertical");
+        dashTime = startDashTime;
+        dashCooldown = new CoolDownHability(cooldownTime);
     }
 
     void FixedUpdate()
     {
-        rb2d.MovePosition(rb2d.position + movement * speed * Time.deltaTime); 
+
+        if (!isDashing)
+        {
+            moveHorizontal = Input.GetAxis("Horizontal");
+            moveVertical = Input.GetAxis("Vertical");
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+            if (movement != Vector2.zero)
+            {
+                rb2d.MovePosition(rb2d.position + movement * speed * Time.deltaTime);
+
+                //Dash
+                if (Input.GetKeyDown(KeyCode.Space) && dashCooldown.IsCoolDownCompleted)
+                {
+                    isDashing = true;
+                    //Instantiate(DashEffect, transform.position, Quaternion.identity);
+                    dashCooldown.SetCooldown();
+                }
+            }
+        }
+        else
+        {
+            if (dashTime < 0)
+            {
+                //the dash has just finished
+                isDashing = false;
+                dashTime = startDashTime;
+                rb2d.velocity = Vector3.zero;
+            }
+            else
+            {
+                //is dashing
+                dashTime -= Time.deltaTime;
+
+                Vector2 movement = new Vector2(System.Math.Sign(moveHorizontal) , System.Math.Sign(moveVertical) );
+                movement.Normalize();
+                rb2d.MovePosition(rb2d.position + movement * dashSpeed * Time.deltaTime);
+            }
+        }
     }
 }
