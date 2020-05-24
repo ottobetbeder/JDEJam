3b86e40490af79a4d1fe6530eed61a19ff05990b
Otto <ottobetbeder@yahoo.com.ar>
2020-05-24 03:19:37 -0300

    IntroSceen Added

---
+++ Assets/Code/IntroSceen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroSceen : MonoBehaviour
+{
+    public GameObject rays;
+
+    public void StartButtonPressed()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        rays.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("SampleScene");
+    }
+}
