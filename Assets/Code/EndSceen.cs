9aaf87e9a9b9b610db1f210cacc1c368e0b52a5b
Otto <ottobetbeder@yahoo.com.ar>
2020-05-24 17:12:17 -0300

    Win sceen and return to intro sceen after a some time

---
+++ Assets/Code/EndSceen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndSceen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ResetGame());
+    }
+    IEnumerator ResetGame()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("Intro");
+    }
+}
