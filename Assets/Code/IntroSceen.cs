2d9b2707d63ab80c1cd949bd5dc953ccaf7b5145
Otto <ottobetbeder@yahoo.com.ar>
2020-05-26 20:12:32 -0300

    Added menu to IntroSceen

--- Assets/Code/IntroSceen.cs
+++ Assets/Code/IntroSceen.cs
@@ -7,9 +7,23 @@ public class IntroSceen : MonoBehaviour
 {
     public GameObject rays;
     public AudioSource buttonClickedSound;
+    public GameObject InstructionPanel;
+    private int instPanelOn = -1;
+
     public void StartButtonPressed()
     {
         StartCoroutine(SpawnEnemies());
+    }
+
+    public void ExitButtonPressed()
+    {
+        Application.Quit();
+    }
+
+    public void InstructionButtonPressed()
+    {
+        instPanelOn *= -1;
+        InstructionPanel.SetActive(instPanelOn == 1);
     }
 
     IEnumerator SpawnEnemies()
